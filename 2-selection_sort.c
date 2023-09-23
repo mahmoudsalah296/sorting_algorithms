648c4e033dafae9010750b2ad73682c6b4d0e524
Mahmoud Salah <ms685116@gmail.com>
2023-09-23 14:33:59 +0300

    algorithms

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -22,22 +22,22 @@ void swap(int *a, int *b)
 */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, min_index;
+	size_t i, j, min_index;
 
-    if (array == NULL || size < 2)
-        return;
-    for (i = 0; i < size - 1; i++)
-    {
-        min_index = i;
-        for (j = i + 1; j < size; j ++)
-        {
-            if (array[j] < array[min_index])
-                min_index = j;
-        }
-        if (min_index != i)
-        {
-            swap(&array[i], &array[min_index]);
-            print_array(array, size);
-        }
-    }
+	if (array == NULL || size < 2)
+		return;
+	for (i = 0; i < size - 1; i++)
+	{
+		min_index = i;
+		for (j = i + 1; j < size; j ++)
+		{
+			if (array[j] < array[min_index])
+				min_index = j;
+		}
+		if (min_index != i)
+		{
+			swap(&array[i], &array[min_index]);
+			print_array(array, size);
+		}
+	}
 }
